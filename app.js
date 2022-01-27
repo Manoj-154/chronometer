244bcc4d5f81d95a675f85d481be8bb3d6acc329
manoj <manojpachipala15@gmail.com>
2022-01-27 19:37:06 +0530

    chronometer

---
+++ app.js
@@ -0,0 +1,57 @@
+const timer = document.getElementById('chronometer-holder');
+
+
+var min = 0;
+var sec = 0;
+var stoptime = true;
+
+function startTimer() {
+  if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
+    }
+}
+function stopTimer() {
+  if (stoptime == false) {
+    stoptime = true;
+  }
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+   
+
+    sec = sec + 1;
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    
+
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+    
+
+    timer.innerHTML =  min + ':' + sec;
+
+    setTimeout("timerCycle()", 1000);
+  }
+}
+
+function resetTimer() {
+    timer.innerHTML = '00:00';
+}
+function resetTimer() {
+    timer.innerHTML = '00:00';
+    stoptime = true;
+    
+    sec = 0;
+    min = 0;
+}
